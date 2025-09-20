6df6c42e6888606988a532633359d3bdfc5df887
arthurtenorio0101 <arthurtenorio0101@gmail.com>
2025-09-20 18:09:52 -0300

    Updates

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-// Component exports for easier imports
-export { Header } from './Header';
-export { About } from './About';
-export { Testimonials } from './Testimonials';
-export { ContactForm } from './ContactForm';
-export { FAQ } from './FAQ';
-export { Footer } from './Footer';
